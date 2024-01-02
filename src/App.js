818a1d2828876ba1c40b6e468440246293ef74b1
Han-Wanburhan <Hangster10761@gmail.com>
2024-01-02 16:32:14 +0700

    []

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./page/HomePage";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //   </Routes>
+    // </Router>
+    <HomePage />
   );
 }
 
